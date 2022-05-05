153ecc1753cb3572ee732c7e593b08d5b685d9dc
GilbertAzevedo <gilbertif1995@outlook.com>
2022-05-05 13:00:44 +0000

    Inserindo Bemvindo.cs

---
+++ Aula_0505/Bemvindo.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  public static void Main() {
+    Console.WriteLine("Digite seu nome");
+    string s = Console.ReadLine();
+    Console.WriteLine("Olá, " + s);
+    Console.WriteLine("Seja bem vindo");
+    Console.WriteLine("Seja bem vindo");
+  }
+}
